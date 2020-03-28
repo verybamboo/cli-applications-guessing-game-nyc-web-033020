b349c6a51e23fb5cb72749316b82430e75bdd1ce
George Chen <georgechen1338@gmail.com>
2020-03-28 07:34:12 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,8 +9,7 @@ def run_guessing_game
   
   if input == num.to_s
     puts "You guessed the correct number!"
-  elsif input.downcase == "exit"
-  puts "Goodbye!"
+  
   else
     puts "Sorry! The computer guessed #{num}."
   end
